af0bc02bcb6271905892e958c6ca4228a497e90a
Simen Hasselknippe <simen.hasselknippe@live.com>
2015-11-15 17:20:02 +0100

    Initial project setup

---
+++ Juleverksted/Juleverksted/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Juleverksted.Startup))]
+namespace Juleverksted
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
