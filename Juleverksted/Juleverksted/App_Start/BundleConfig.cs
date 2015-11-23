722f694e1f81555fbab164d6bb77d36c2a9195fe
Simen Hasselknippe <simen.hasselknippe@live.com>
2015-11-23 19:40:14 +0100

    Regex og angular

--- Juleverksted/Juleverksted/App_Start/BundleConfig.cs
+++ Juleverksted/Juleverksted/App_Start/BundleConfig.cs
@@ -23,6 +23,10 @@ namespace Juleverksted
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.js",
+                      "~/Scripts/angular-ui-router.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
--- Juleverksted/Juleverksted/Controllers/HomeController.cs
+++ Juleverksted/Juleverksted/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -20,11 +21,54 @@ namespace Juleverksted.Controllers
 
         public ActionResult Reinsdyr()
         {
-            return View();
+            return View(new Reinsdyrmodel());
         }
         
         public ActionResult Login() {
             return View();
         }
+
+        public ActionResult MyPost(Reinsdyrmodel reinsdyrmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Angular");
+
+            }
+            return View("Reinsdyr", reinsdyrmodel);
+        }
+
+        public ActionResult Angular()
+        {
+            return View();
+        }
+    }
+
+    public class Reinsdyrmodel
+    {
+        [Required(ErrorMessage = "Field can't be empty")]
+        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$", ErrorMessage = "Please Enter Correct Regex")]
+        public string Something0 { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [RegularExpression(@"^\d{5}$", ErrorMessage = "Please Enter Correct Regex")]
+        public string Something1 { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{6,50}$", ErrorMessage = "Please Enter Correct Regex")]
+        public string Something2 { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [RegularExpression(@"^\d{3}-?\d{2}-?\d{4}$", ErrorMessage = "Please Enter Correct Regex")]
+        public string Something3 { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [RegularExpression(@"\b(?:the|and|a|to|of|in|i|is|that|it|on|you|this|for|but|with|are|have|be|at|or|as|was|so|if|out|not)\b", ErrorMessage = "Please Enter Correct Regex")]
+        public string Something4 { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [RegularExpression(@"^(?:[A-Z]\:|\\)(\\[\w\.$]+)+\\?$", ErrorMessage = "Please Enter Correct Regex")]
+        public string Something5 { get; set; }
+
     }
 }
